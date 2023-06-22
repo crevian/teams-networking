75c60bcd52b80f413b0871eb0eb2075621878194
Vlad Crevian <vlad.crevian@gmail.com>
2023-06-22 20:30:56 +0300

    display teams.json into table

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import "./style.css";
 //starting the app
 
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+function getTeamAsHTML(team) {
+  return `<tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td>${team.url}</td>
+    <td>✂ &#9998;</td>
+    </tr>`;
+}
+
+function displayTeams(teams) {
+  const teamsHTML = teams.map(getTeamAsHTML);
+  console.warn("teams", teamsHTML);
+
+  $("#teamsTable tbody").innerHTML = teamsHTML.join("");
+}
+
 function loadTeams() {
   fetch("teams.json")
     .then((r) => r.json())
     .then((teams) => {
-      console.warn("teams", teams);
+      displayTeams(teams);
     });
 }
 
