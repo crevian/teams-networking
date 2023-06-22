43ae8feecd858381b8b2be7866da8ee5aba4320a
Vlad Crevian <vlad.crevian@gmail.com>
2023-06-22 20:07:15 +0300

    simplify js to use chained operations

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import "./style.css";
 //starting the app
 
 function loadTeams() {
-  const request = fetch("teams.json");
-  const response = request.then((r) => r.json());
-  response.then((result) => {
-    console.warn("result", result);
-  });
+  fetch("teams.json")
+    .then((r) => r.json())
+    .then((teams) => {
+      console.warn("teams", teams);
+    });
 }
 
 loadTeams();
