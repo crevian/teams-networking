bf8e1a6aec64bae90036e6291ab930c756b26301
Vlad Crevian <vlad.crevian@gmail.com>
2023-07-03 15:53:18 +0300

    split into getTeamValues, setTeamValues

--- src/index.js
+++ src/index.js
@@ -73,28 +73,35 @@ function loadTeams() {
 
 function startEdit(id) {
   editId = id;
-  const team = allTeams.find(team => team.id == id); //Todo
-  // console.info(team);
+  const team = allTeams.find(team => team.id == id);
+  setTeamValues(team);
+}
 
+function setTeamValues(team) {
   $("#promotion").value = team.promotion;
   $("#members").value = team.members;
   $("input[name=name]").value = team.name;
   $("input[name=url]").value = team.url;
 }
 
-function onSubmit(e) {
-  e.preventDefault();
+function getTeamValues() {
   const promotion = $("#promotion").value;
   const members = $("#members").value;
   const name = $("input[name=name]").value;
   const url = $("input[name=url]").value;
-  const team = {
-    // id: editId,
+  return {
     promotion,
     members,
     name: name,
     url: url
   };
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const team = getTeamValues();
+
   if (editId) {
     team.id = editId;
     console.warn("submit", team);
