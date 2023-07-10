b0cc616f31ac22eb5b01bc85068e0c4641a6bef0
Vlad Crevian <vlad.crevian@gmail.com>
2023-07-10 15:54:13 +0300

    allow to pass String or Element on mask/unmask

--- src/utilities.js
+++ src/utilities.js
@@ -2,12 +2,27 @@ export function $(selector) {
   return document.querySelector(selector);
 }
 
+/**
+ *
+ * @param {String|Element} el
+ */
 export function mask(el) {
+  if (typeof el === "string") {
+    el = $(el);
+  }
+
   //   $("#teamsForm").classList.add("loading-mask");
   el.classList.add("loading-mask");
 }
 
+/**
+ *
+ * @param {String|Element} el
+ */
 export function unmask(el) {
+  if (typeof el === "string") {
+    el = $(el);
+  }
   //   $("#teamsForm").classList.remove("loading-mask");
   el.classList.remove("loading-mask");
 }
@@ -26,3 +41,14 @@ export function sleep(ms) {
   console.warn("ready to do %o", "next job");
   console.warn("executed before sleep.then");
 })();
+
+export function filterElements(elements, search) {
+  search = search.toLowerCase();
+  return elements.filter(element => {
+    return Object.entries(element).some(([key, value]) => {
+      if (key !== "id") {
+        return value.toLowerCase().includes(search);
+      }
+    });
+  });
+}
